763b2ff0baf254dfc425b2b0d630f91b5406586b
Cristian Beraha <cristian@cloudops.works>
2026-02-26 17:35:17 -0300

    chore: Increase PostgreSQL provider connection timeout to 60 seconds

--- provider-pgsql.tf
+++ provider-pgsql.tf
@@ -89,6 +89,6 @@ provider "postgresql" {
   database          = local.psql.db_name
   database_username = local.psql.admin_user
   sslmode           = local.psql.sslmode
-  connect_timeout   = 15
+  connect_timeout   = 60
   superuser         = local.psql.superuser
 }
