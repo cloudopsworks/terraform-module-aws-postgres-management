6a17c02e4543a7668803a9e98616ca80101c8e17
Cristian Beraha <cristian@cloudops.works>
2026-02-26 11:51:38 -0300

    chore: Cleanup unused AWS resources and update provider versions
    - Removed redundant AWS RDS and Secrets Manager resource data blocks.
    - Updated AWS provider version to `~> 6.4`.
    - Fixed `system_name_short` logic to handle environment type formatting.
    - Added `.cloudopsworks` directory with provider and configuration placeholders.

--- aws.tf
+++ aws.tf
@@ -6,34 +6,5 @@
 #       WebSite: https://cloudops.works
 #     Distributed Under Apache v2.0 License
 #
+
 data "aws_region" "current" {}
-
-data "aws_db_instance" "db" {
-  count                  = try(var.rds.enabled, false) && !try(var.rds.cluster, false) ? 1 : 0
-  db_instance_identifier = nonsensitive(try(var.rds.name, local.from_secret["dbInstanceIdentifier"]))
-}
-
-data "aws_rds_cluster" "db" {
-  count              = try(var.rds.enabled, false) && try(var.rds.cluster, false) ? 1 : 0
-  cluster_identifier = nonsensitive(try(var.rds.name, local.from_secret["dbClusterIdentifier"]))
-}
-
-data "aws_db_instance" "hoop_db_server" {
-  count                  = local.hoop_connect && !try(var.hoop.cluster, false) ? 1 : 0
-  db_instance_identifier = var.hoop.server_name
-}
-
-data "aws_rds_cluster" "hoop_db_server" {
-  count              = local.hoop_connect && try(var.hoop.cluster, false) ? 1 : 0
-  cluster_identifier = var.hoop.server_name
-}
-
-data "aws_secretsmanager_secret" "db_password" {
-  count = try(var.rds.enabled, false) || try(var.rds.from_secret, false) ? 1 : 0
-  name  = var.rds.secret_name
-}
-
-data "aws_secretsmanager_secret_version" "db_password" {
-  count     = try(var.rds.enabled, false) || try(var.rds.from_secret, false) ? 1 : 0
-  secret_id = data.aws_secretsmanager_secret.db_password[0].id
-}
